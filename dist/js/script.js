b671c633ba0ff59929c09bc2e4bc5014436c758d
Ruslan Akhmetshin <184346@mail.ru>
2020-05-10 01:29:37 +0300

    some fixes - smooth scrolling, fake server for form, mask for input(phone) added, checking for filling in input fields added, AJAX POST added, design for the state of buttons and links added, 3 ways to close the form added, address tags added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,62 @@
 $(function() {
 	$('.fly-elements').addClass('fly-elements--active');
 
-	$('html').on('click','.header__toggle, .menu__link', function() {
+
+  $('.contact-form__input--phone').mask('+7 (999) 999-99-99');
+
+
+  $("form").submit(function(){
+    var $form = $(this);
+    $form.find('.error').removeClass('error');
+
+    if ($form.find('input[name=name]').val() == '' |
+    $form.find('input[name=name]').val() == 'Пожалуйста, введите имя') {
+        $form.find('input[name=name]').addClass('error');
+        $form.find('input[name=name]').val('Пожалуйста, введите имя');
+        $('.contact-form__button--call:focus').css('box-shadow','0 0 5px 5px red');
+        return false;
+    }
+    if ($form.find('input[name=phone]').val() == '' |
+    $form.find('input[name=phone]').val() == 'Пожалуйста, введите телефон') {
+        $form.find('input[name=phone]').addClass('error');
+        $form.find('input[name=phone]').val('Пожалуйста, введите телефон');
+        $('.contact-form__button--call:focus').css('box-shadow','0 0 5px 5px red');
+        return false;
+    }
+    else {
+      $('.contact-form__button--call:focus').css('box-shadow','0 0 5px 5px green');
+    }
+
+    $.post(
+        $form.attr("action"),
+        $form.serialize()
+    );
+    $('.contact-form').fadeOut();
+
+    return false;
+  });
+
+  $('.contact-form__input--name').focus(function(){
+    $(this).select();
+  });
+
+  $('html').on('click','.contact-form__button--cancel', function() {
+      $('.contact-form').fadeOut();
+  });
+  $('html').keydown(function(e) {
+    if (e.keyCode == 27) {
+      e.stopPropagation();
+      $('.contact-form').fadeOut();
+    }
+  });
+  $('.contact-form').click(function(e) {
+    if ($(e.target).closest('.contact-form__form').length == 0) {
+      $(this).fadeOut();          
+    }
+  });
+
+
+	$('html').on('click','.header__toggle, .menu__link--header', function() {
 		if ($('.header__phone').css('margin-right') == '25px') {
 			$('.header__menu').toggleClass('header__menu--active');
 			$('.menu__list').toggleClass('header__menu--active');
@@ -12,9 +67,11 @@ $(function() {
 		}
 	});
 
+
 	if ($(window).width() < 639) {
 		$('.footer__call-order').addClass('button--orange-style');
 	}
+
 
   $('html').on('click','.button--call-order', function() {
       $('.contact-form').css('display','block');
@@ -30,10 +87,8 @@ $(function() {
       $('.contact-form__headers--call-order').css('display','none');
       $('.contact-form__labels--email').css('display','block');
   });
-  $('html').on('click','.contact-form__button--cancel', function() {
-      $('.contact-form').css('display','none');
-  });
 });
+
 
 new Swiper('.examples__slides', {
   slidesPerView: 1,
